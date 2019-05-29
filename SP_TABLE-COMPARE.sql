7cba64b04027bc96bc2bae34c1dbaee00a80ba3e
Maximiliano De Pietro <elwindwalker@gmail.com>
2019-05-29 20:35:31 -0300

    Tablas temporales para comparar colummnas

--- SP_TABLE-COMPARE.sql
+++ SP_TABLE-COMPARE.sql
@@ -13,9 +13,29 @@ BEGIN TRY
 		RAISERROR(N'No existe la base de datos %s', 16, 1, @origin_database)
 	IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE NAME = @destination_database)
 		RAISERROR(N'No existe la base de datos %s', 16, 1, @destination_database)
+-- Traer todas las columnas de la tabla de origen
+CREATE TABLE #columnasTablaOrigen (COLUMN_NAME VARCHAR(80))
+(
 	SELECT COLUMN_NAME
 	FROM INFORMATION_SCHEMA.COLUMNS
-		WHERE TABLE_NAME = @origin_database
+		WHERE TABLE_NAME = @origin_database + '.' + @table
+);
+-- Traer todas las columnas de tabla destino
+CREATE TABLE #columnasTablaDestino (COLUMN_NAME VARCHAR(80))
+(
+	SELECT COLUMN_NAME
+	FROM INFORMATION_SCHEMA.COLUMNS
+		WHERE TABLE_NAME = @destination_database + '.' + @table
+);
+-- Traer columnas que no existan en la de destino
+CREATE TABLE #columnasHuerfanas (COLUMN_NAME VARCHAR(80))(
+	SELECT * FROM #columnasTablaOrigen AS ori 
+	LEFT JOIN #columnasTablaDestino AS dest ON ori.COLUMN_NAME = dest.COLUMN_NAME
+		WHERE dest.COLUMN_NAME = NULL
+);
+IF (SELECT COUNT(COLUMN_NAME) FROM #columnasHuerfanas) > 0
+	-- Crear el SP (o meter acá) para recorrer, cursor mediante, estas columnas para realizar el alter correspondiente en la tabla destino
+	RAISERROR('Not implemented', 16, 1)
 END TRY
 BEGIN CATCH
 END CATCH
