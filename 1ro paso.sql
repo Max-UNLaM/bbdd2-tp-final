3784ed17d8c8da7c55646e1d2dd0cbf56acb6e38
moises <dotty@live.com.ar>
2019-06-18 05:06:46 -0300

    tablas temporales origen y destino creado

--- 1ro paso.sql
+++ 1ro paso.sql
@@ -1,22 +1,35 @@
-create database Z
+CREATE DATABASE Z -- creo para ahi guardar los SP (por ahora)
+USE Z 
 
-alter proc SP_Compare @db_origen sysname, @db_destino sysname
-as
-	if exists(select 1 from sys.databases where name=@db_origen and name=@db_destino)
-	begin
-		/*Enpiezo a comparar las tablas*/
-		print 'ok!!!'
-	end
+ALTER PROC SP_CompareDB(@db_origen NVARCHAR(100), @db_destino NVARCHAR(100))
+AS
+BEGIN
+	IF (exists(SELECT 1 FROM sys.databases WHERE name=@db_origen) and
+		exists(SELECT 1 FROM sys.databases WHERE name=@db_destino))
+		BEGIN
+			DECLARE @query NVARCHAR(1000)
+			--Obtengo las tablas del db_origen y guardo en la tabla temporal
+			SET @query = 'SELECT TABLE_CATALOG, TABLE_NAME INTO ##TABLAS_BD_ORIGEN FROM '+@db_origen+'.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE =''BASE TABLE'';'
+			EXEC (@query)
+			--Obtengo las tablas del db_destino y guardo en la tabla temporal
+			SET @query = 'SELECT TABLE_CATALOG, TABLE_NAME INTO ##TABLAS_BD_DESTINO FROM '+@db_destino+'.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE =''BASE TABLE'';'
+			EXEC (@query)
 
-	else
-	begin 
-		if not exists(select 1 from sys.databases where name=@db_origen)
-		RAISERROR(N'No existe la base de datos %s', 16, 1, @db_origen)
+		END
+	ELSE
+		BEGIN 
+			if not exists(select 1 from sys.databases where name=@db_origen)
+			RAISERROR(N'No existe la base de datos %s', 16, 1, @db_origen)
 	
-		if not exists(select 1 from sys.databases where name=@db_destino)
-		RAISERROR(N'No existe la base de datos %s', 16, 1, @db_destino)
-	end
+			if not exists(select 1 from sys.databases where name=@db_destino)
+			RAISERROR(N'No existe la base de datos %s', 16, 1, @db_destino)
+		END
+END
+
+EXEC SP_CompareDB 'origen', 'destino'
+
+
+-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
-exec SP_Compare 'origen', 'destino'
